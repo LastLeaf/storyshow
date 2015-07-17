6176eb04b6c054ffea14f86b90f4eb4335ff4371
unknown <v-bofu@suzvdi-031.fareast.corp.microsoft.com>
2015-07-17 18:23:04 +0800

    Added initial project files.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fs = require('fs');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+
+var paths = {
+	scripts: ['src/*.js']
+};
+
+gulp.task('clean', function(cb){
+	del(['bin'], cb);
+});
+
+gulp.task('scripts', ['clean'], function(){
+	return gulp.src(paths.scripts)
+		.pipe(sourcemaps.init())
+			.pipe(uglify())
+			.pipe(concat('storyshow.min.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('bin'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('default', ['scripts']);
---
+++ src/init.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function(){
+	this.StoryShow = function(){};
+})(this);
